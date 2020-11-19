1b3e536a233dff24093e1c45afcd635174495954
dohun6643 <ehgns6643@gmail.com>
2020-11-19 10:21:08 +0900

    11/18

--- DisplayWithPoint.cpp
+++ DisplayWithPoint.cpp
@@ -8,9 +8,9 @@ int main(){
     using namespace std;
     int max_y = LINES-1, max_x =COLS-COLS;
     printw("Start Hello Ncurse!");     //start main
-    move(3,4);  addch('5'); mvaddch(5,4,'5');
+    move(10,4);  addch('7'); mvaddch(5,4,'5');
     string str_var = "max_y,max_x ;" + to_string(int(LINES))+","+to_string(int(COLS));
-    mvaddstr(10,8,str_var.c_str());
+    mvaddstr(20,15,str_var.c_str());
     mvprintw(max_y/2,COLS/2,"Middle Hello Ncurse!");
     mvaddch(max_y -1,COLS -1,'E');
     printw("Hello Ncurse!");
--- Gamemap.cpp
+++ Gamemap.cpp
@@ -8,7 +8,7 @@ int main(){
     curs_set(0);    //visible cursor
     using namespace std;
     int max_y = LINES-1, max_x =COLS-1;
-    // string str_var = "max_y,max_x ;" + to_string(int(LINES))+"&"+to_string(int(COLS));
+    string str_var = "max_y,max_x ;" + to_string(int(LINES))+"&"+to_string(int(COLS));
     printw("*************************************************************************");    //start main
     move(1,0);  addch('*'); mvaddch(5,0,'*');mvaddch(10,0,'*');mvaddch(15,0,'*');mvaddch(20,0,'*');mvaddch(25,0,'*');
     move(1,72);  addch('*'); mvaddch(5,72,'*');mvaddch(10,72,'*');mvaddch(15,72,'*');mvaddch(20,72,'*');mvaddch(25,72,'*');    
